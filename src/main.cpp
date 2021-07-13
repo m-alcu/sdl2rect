5bd2e1030c1132d18405c990dce5dc9d724bfd34
m-alcu <martin.alcubierre@gmail.com>
2021-07-13 22:39:13 +0200

    Include framerate, wait Vertical retrace (16 ms)

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <string> 
 #include "poly.h"
 
 int main(int argv, char** args)
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	//SDL_Init(SDL_INIT_EVERYTHING);
+	if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cerr << "Unable to initialize SDL: " << SDL_GetError() << std::endl;
+        return -1;
+    }
 
 	Screen screen;
 	screen.width = 800;
 	screen.high = 600;
 
-	SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen.width, screen.high, 0);
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Window *window = SDL_CreateWindow("Poly3d", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen.width, screen.high, 0);
+	//SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 
+                                                SDL_RENDERER_ACCELERATED | 
+                                                SDL_RENDERER_PRESENTVSYNC);
 	SDL_Texture * texture = SDL_CreateTexture(renderer,
         SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, screen.width, screen.high);
 	Uint32 * pixels = new Uint32[screen.width * screen.high];
@@ -25,6 +34,9 @@ int main(int argv, char** args)
 	Uint32 color;
 	Rectangle rectangle;
 	Triangle triangle;
+
+	Uint32 ant;
+	Uint32 dif;
 	
 
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -50,15 +62,20 @@ int main(int argv, char** args)
 			}
 		}
 		
-		for(int i=0;i<100;i++) {
+		for(int i=0;i<1;i++) {
 			//rectangle.randomDraw(pixels, screen);
 			triangle.randomDraw(pixels, screen);
 		}
 
-		SDL_SetWindowTitle(window, "pongo el titulo");
+		dif = SDL_GetTicks() - ant;
+		ant += dif;
+
+		std::string s = std::to_string(dif);
+		char const *num_char = s.c_str();
+		SDL_SetWindowTitle(window, num_char);
 		SDL_UpdateTexture(texture, NULL, pixels, screen.width * sizeof(Uint32));
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);		
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
 		SDL_RenderPresent(renderer);
 	}
 
