9783afe4578e6d50d24eed885e6fd65fdcb984ca
SECTORIALES\malcubierre <martin.alcubierre@inetum.com>
2025-03-17 15:40:21 +0100

    Move values to class

--- src/backgrounds/background.hpp
+++ src/backgrounds/background.hpp
@@ -4,11 +4,6 @@
 
 class Background {
 
-    public:
-        uint16_t seed1;
-        uint16_t seed2;
-
-
     protected:
         // Protected virtual methods to be implemented by derived classes.
         virtual void draw(uint32_t *pixels, uint16_t high, uint16_t width) = 0;
--- src/backgrounds/desert.hpp
+++ src/backgrounds/desert.hpp
@@ -6,6 +6,10 @@
 class Desert : public Background {
 
     public:
+        uint16_t seed1;
+        uint16_t seed2;
+
+    public:
         void draw(uint32_t *pixels, uint16_t high, uint16_t width);
 
     private:
