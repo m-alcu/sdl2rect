ec6e10cc9991cfea4c4961d0ae832ff7c19f2e6b
SECTORIALES\malcubierre <martin.alcubierre@inetum.com>
2025-03-17 19:38:29 +0100

    Fix comment

--- src/backgrounds/background.hpp
+++ src/backgrounds/background.hpp
@@ -6,7 +6,6 @@
 class Background {
 
     public:
-        // Protected virtual methods to be implemented by derived classes.
         virtual void draw(uint32_t *pixels, uint16_t high, uint16_t width) = 0;
 
         virtual ~Background() {}
