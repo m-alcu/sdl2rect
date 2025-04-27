e1f96e702348ca5858db56cbabf8735a508a1dbe
SECTORIALES\malcubierre <martin.alcubierre@inetum.com>
2025-04-27 20:43:43 +0200

    .

--- src/rasterizer.hpp
+++ src/rasterizer.hpp
@@ -228,58 +228,6 @@ class Rasterizer {
                 right += rightDy;
             }
         };
-
-        inline uint32_t PhongPixelShader(const vertex& gRaster, const Scene& scene, const Face& face) {
-
-            slib::vec3 normal = smath::normalize(gRaster.normal);
-            float diff = std::max(0.0f, smath::dot(normal,scene.lux));
-        
-            slib::vec3 R = smath::normalize(normal * 2.0f * smath::dot(normal,scene.lux) - scene.lux);
-            float specAngle = std::max(0.0f, smath::dot(R,scene.eye)); // viewer
-            float spec = std::pow(specAngle, face.material.Ns);
-        
-            if (spec > 0.95) { 
-                return 0xffffffff; // White point if the light is too close to the normal
-            }
-        
-            float r, g, b;
-            // Calculate brightness
-            r = std::min(face.material.Ka[0] + face.material.Kd[0] * diff + face.material.Ks[0] * spec, 255.0f);
-            g = std::min(face.material.Ka[1] + face.material.Kd[1] * diff + face.material.Ks[1] * spec, 255.0f);
-            b = std::min(face.material.Ka[2] + face.material.Kd[2] * diff + face.material.Ks[2] * spec, 255.0f);
-        
-            return Color(b, g, r).toBgra();  // Create a color object with the calculated RGB values and full alpha (255)
-        }
-        
-        inline uint32_t BlinnPhongPixelShader(const vertex& gRaster, const Scene& scene, const Face& face) {
-        
-            // Normalize vectors
-            slib::vec3 N = smath::normalize(gRaster.normal); // Normal at the fragment
-            slib::vec3 L = scene.lux; // Light direction
-            slib::vec3 V = scene.eye; // Viewer direction (you may want to define this differently later)
-        
-            // Diffuse component
-            float diff = std::max(0.0f, smath::dot(N,L));
-        
-            // Halfway vector H = normalize(L + V)
-            //slib::vec3 H = smath::normalize(L + V);
-        
-            // Specular component: spec = (N · H)^shininess
-            float specAngle = std::max(0.0f, smath::dot(N,scene.halfwayVector)); // viewer
-            float spec = std::pow(specAngle, face.material.Ns); // Blinn Phong shininess needs *4 to be like Phong
-        
-            if (spec > 0.95) { 
-                return 0xffffffff; // White point if the light is too close to the normal
-            }
-        
-            float r, g, b;
-            // Calculate brightness
-            r = std::min(face.material.Ka[0] + face.material.Kd[0] * diff + face.material.Ks[0] * spec, 255.0f);
-            g = std::min(face.material.Ka[1] + face.material.Kd[1] * diff + face.material.Ks[1] * spec, 255.0f);
-            b = std::min(face.material.Ka[2] + face.material.Kd[2] * diff + face.material.Ks[2] * spec, 255.0f);
-        
-            return Color(b, g, r).toBgra();  // Create a color object with the calculated RGB values and full alpha (255)
-        }
         
         bool IsInside(const vertex& v, ClipPlane plane) {
             const auto& p = v.ndc;
