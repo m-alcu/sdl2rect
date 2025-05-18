a16788a75f8b33247ad6bfc108448a98d953fa35
SECTORIALES\malcubierre <martin.alcubierre@inetum.com>
2025-05-18 19:33:53 +0200

    Simplify Flat

--- src/effects/TexturedFlatEffect.hpp
+++ src/effects/TexturedFlatEffect.hpp
@@ -12,26 +12,25 @@ public:
 	public:
     Vertex() {}
 
-    Vertex(int32_t px, int32_t py, float pz, slib::vec3 n, slib::vec4 vp, slib::zvec2 _tex) :
-    p_x(px), p_y(py), p_z(pz), normal(n), ndc(vp), tex(_tex) {}
+    Vertex(int32_t px, int32_t py, float pz, slib::vec4 vp, slib::zvec2 _tex) :
+    p_x(px), p_y(py), p_z(pz), ndc(vp), tex(_tex) {}
 
     Vertex operator+(const Vertex &v) const {
-        return Vertex(p_x + v.p_x, p_y + v.p_y, p_z + v.p_z, normal + v.normal, ndc + v.ndc, tex + v.tex);
+        return Vertex(p_x + v.p_x, p_y + v.p_y, p_z + v.p_z, ndc + v.ndc, tex + v.tex);
     }
 
     Vertex operator-(const Vertex &v) const {
-        return Vertex(p_x - v.p_x, p_y - v.p_y, p_z - v.p_z, normal - v.normal, ndc - v.ndc, tex - v.tex);
+        return Vertex(p_x - v.p_x, p_y - v.p_y, p_z - v.p_z, ndc - v.ndc, tex - v.tex);
     }
 
     Vertex operator*(const float &rhs) const {
-        return Vertex(p_x * rhs, p_y * rhs, p_z * rhs, normal * rhs, ndc * rhs, tex * rhs);
+        return Vertex(p_x * rhs, p_y * rhs, p_z * rhs, ndc * rhs, tex * rhs);
     }
 
     Vertex& operator+=(const Vertex &v) {
         p_x += v.p_x;
         p_y += v.p_y;
         p_z += v.p_z;
-        normal += v.normal;
         ndc += v.ndc;
         tex += v.tex;
         return *this;
@@ -43,7 +42,6 @@ public:
         float p_z; 
         slib::vec3 world;
         slib::vec3 point;
-        slib::vec3 normal;
         slib::vec4 ndc;
         slib::zvec2 tex; // Texture coordinates
 	};
